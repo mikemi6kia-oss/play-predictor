a4eb802e4088b6c46dd4950c3bfe7cc32d50ee5c
mikemi6kia-oss <mikemi6kia@gmail.com>
2026-03-19 14:47:07 -0400

    Update streamlit_app_pro.py

--- streamlit_app_pro.py
+++ streamlit_app_pro.py
@@ -263,7 +263,7 @@ def scan_high_confidence_tendencies():
     return df
 
 def build_reliable_presets():
-if st.button("Generate top 5 tendencies"):
+st.button("Generate top 5 tendencies"):
     scan_df = scan_high_confidence_tendencies()
     
     if scan_df.empty:
