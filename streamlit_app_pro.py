0e65436ca81799824f7c0859c5efe1e9d19da81b
mikemi6kia-oss <mikemi6kia@gmail.com>
2026-03-23 15:48:16 -0400

    Update streamlit_app_pro.py

--- streamlit_app_pro.py
+++ streamlit_app_pro.py
@@ -423,32 +423,34 @@ logo_html = ""
 if logo_b64:
     logo_html = f'<img class="team-logo" src="data:image/png;base64,{logo_b64}" alt="{team_name} logo" />'
 
-st.markdown(f"""
+banner_html = textwrap.dedent(f"""
 <div class="main-banner">
-    <div class="banner-grid">
-        <div>
-            <div style="font-size:1.7rem; font-weight:700;">CFL Play Frequency Outliers</div>
-            <div style="opacity:0.92; margin-top:0.25rem;">
-                A data-driven model used to anticipate offensive tendencies.
-            </div>
-        </div>
-
-        <div class="banner-center">
-            {logo_html}
-            <div>
-                <div class="banner-team-name">{team_name}</div>
-                <div class="banner-team-sub">Selected scouting profile</div>
-            </div>
-        </div>
-
-        <div class="banner-right">
-            <span class="small-pill">Model: {METRICS['model_type']}</span>
-            <span class="small-pill">2024 CFL Data</span>
-            <span class="small-pill">v10.20250323</span>
-        </div>
-    </div>
+<div class="banner-grid">
+<div>
+<div style="font-size:1.7rem; font-weight:700;">CFL Play Frequency Outliers</div>
+<div style="opacity:0.92; margin-top:0.25rem;">
+A data-driven model used to anticipate offensive tendencies.
 </div>
-""", unsafe_allow_html=True)
+</div>
+
+<div class="banner-center">
+{logo_html}
+<div>
+<div class="banner-team-name">{team_name}</div>
+<div class="banner-team-sub">Selected scouting profile</div>
+</div>
+</div>
+
+<div class="banner-right">
+<span class="small-pill">Model: {METRICS['model_type']}</span>
+<span class="small-pill">2024 CFL Data</span>
+<span class="small-pill">v10.20250323</span>
+</div>
+</div>
+</div>
+""")
+
+st.markdown(banner_html, unsafe_allow_html=True)
 
 with st.sidebar:
     st.header("Game Inputs")
